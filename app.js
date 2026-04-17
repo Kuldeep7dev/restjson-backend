b62fdd2d1bbdcac97151b644c81f1e8df08985e5
Kuldeep7devi <kushallaxkar9@gmail.com>
2026-04-17 15:21:32 +0530

    Change clent url

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL ? process.env.CLIENT_URL.replace(/\/$/, '') : undefined,
   'http://localhost:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
 ];
 
 app.use(cors({
